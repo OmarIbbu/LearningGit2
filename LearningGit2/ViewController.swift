3a6574d87713e4f54bc7a51a82cee44e14ee5ee8
thincnext <thincnext@deadlyStation.local>
2018-09-02 14:30:44 +0530

    Versin 1

---
+++ LearningGit2/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  LearningGit2
+//
+//  Created by thincnext on 02/09/18.
+//  Copyright © 2018 thincnext. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func showName(){
+        print("My Name is omar")
+    }
+
+
+}
+
